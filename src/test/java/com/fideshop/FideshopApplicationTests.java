3ee1e2cd4df02b4a74845e0ed18c8bd51ac7cac9
Ronal <Ronal@DesktopRD>
2023-02-05 12:24:22 -0600

    Primera Tarea, Semana 3

---
+++ src/test/java/com/fideshop/FideshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fideshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FideshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
